a9da9197c441577b9c33c21097608267d9615798
Nkemjika1divine <cndivine@gmail.com>
2024-06-12 14:30:39 -0300

    user.py updated with the user model

---
+++ models/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""The User module"""
+from models.basemodel import BaseModel, Base
+from sqlalchemy import Column, String
+
+
+class User(BaseModel, Base):
+    """The User class"""
+    __tablename__ = "users"
+    name = Column(String(50), nullable=False)
+    username = Column(String(20), nullable=False, unique=True)
+    email = Column(String(50), nullable=False, unique=True)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
