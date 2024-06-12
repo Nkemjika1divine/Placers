d4d06dc5635cf95af014c74637ed61737076ac82
Nkemjika1divine <cndivine@gmail.com>
2024-06-12 09:58:05 -0300

    basemodel.py updated with BaseModel class attributes

--- models/basemodel.py
+++ models/basemodel.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""The BaseModel Module"""
+from datetime import datetime
+from sqlalchemy import Column, String, DateTime
+
+
+class BaseModel:
+    """The BaseModel class"""
+    __abstract__ = True
+    id = Column(String(50), primary_key=True, nullable=False)
+    time_created = Column(DateTime, default=datetime.now, nullable=False)
+    time_updated = Column(DateTime, default=datetime.now, nullable=False)
