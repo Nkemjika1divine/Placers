9b2252eeed4a0236d7f7f1f02edd2d81db739965
Nkemjika1divine <cndivine@gmail.com>
2024-06-12 19:59:23 -0300

    review.py updated with the review model; place.py updated

--- models/place.py
+++ models/place.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
 """The Place Module"""
+from decimal import Decimal
 from models.basemodel import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, DECIMAL
+from sqlalchemy import Column, String, ForeignKey, CheckConstraint
+
+
+categories = ["Religious institution",
+              "School",
+              "Park",
+              "Hotel",
+              "Restaurant",
+              "Museum",
+              "Farm",
+              "Industry",
+              "Studio",
+              "Beach",
+              "Club"]
 
 
 class Place(BaseModel, Base):
@@ -9,10 +23,10 @@ class Place(BaseModel, Base):
     __tablename__ = "places"
     creator_id = Column(String(50), ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     name = Column(String(50), nullable=False)
-    category = Column(String(50), nullable=False)
+    category = Column(String(50), CheckConstraint("category IN categories"),nullable=False)  # To be fixed
     address = Column(String(250), nullable=False, unique=True)
-    longitude = Column(DECIMAL(10, 6), nullable=True)
-    latitude = Column(DECIMAL(10, 6), nullable=True)
+    longitude = Column(Decimal(10, 6), nullable=True)
+    latitude = Column(Decimal(10, 6), nullable=True)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- models/review.py
+++ models/review.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 """The Review module"""
 from models.basemodel import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey, CheckConstraint, Integer
+
+
+class Review(BaseModel, Base):
+    """The Review model"""
+    __tablename__ = "reviews"
+    user_id = Column(String(50), ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    place_id = Column(String(50), ForeignKey("places.id", ondelete="CASCADE"), nullable=False)
+    rating = Column(Integer, CheckConstraint("rating >= 0 AND rating <= 10"), nullable=False)
+    review = Column(String(200), nullable=True)
