773ee24b12d3e706248bcca4b4e4474ba22b5509
Nkemjika1divine <cndivine@gmail.com>
2024-06-12 10:20:06 -0300

    basemodel.py updated with to_dict() in the BaseModel class

--- models/basemodel.py
+++ models/basemodel.py
@@ -28,6 +28,18 @@ class BaseModel:
             self.time_created = datetime.now()
             self.time_updated = datetime.now()
     
+    def to_dict(self):
+        """Returns a dictionary representation of the object"""
+        copy = self.__dict__.copy()
+        copy['__class__'] = self.__class__.__name__
+        if 'time_created' in copy:
+            copy['time_created'] = self.time_created.isoformat()
+        if 'time_updated' in copy:
+            copy['time_updated'] = self.time_updated.isoformat()
+        if "_sa_instance_state" in copy:
+            del copy["_sa_instance_state"]
+        return copy
+    
     def __str__(self):
         """Returns a string representation of the object"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
