43a21723a200a7eac80019412b704e2534b94625
Nkemjika1divine <cndivine@gmail.com>
2024-06-12 10:01:22 -0300

    basemodel.py updated with the __init__() of the BaseModel class

--- models/basemodel.py
+++ models/basemodel.py
@@ -2,6 +2,7 @@
 """The BaseModel Module"""
 from datetime import datetime
 from sqlalchemy import Column, String, DateTime
+from uuid import uuid4
 
 
 class BaseModel:
@@ -10,3 +11,19 @@ class BaseModel:
     id = Column(String(50), primary_key=True, nullable=False)
     time_created = Column(DateTime, default=datetime.now, nullable=False)
     time_updated = Column(DateTime, default=datetime.now, nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        """Initializing the Basemodel class"""
+        if 'id' not in kwargs:
+            self.id = str(uuid4())
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'time_created' or key == 'time_updated':
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.time_created = datetime.now()
+            self.time_updated = datetime.now()
