327775aecfccfa2fd056035edcb9e730a3cd60c7
Nkemjika1divine <cndivine@gmail.com>
2024-06-14 22:00:59 -0300

    API route: GET /status fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 """Module deploying our FastAPI app"""
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+# from api.v1.endpoints.index import index_router
 
 
 app = FastAPI()
+
+
+index_router = APIRouter()
+
+
+api_prefix = "/api/v1"
+
+
+app.include_router(index_router, prefix=api_prefix)
--- api/v1/endpoints/index.py
+++ api/v1/endpoints/index.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
 """Module conntaining index endpoints"""
-from api.v1.app import app
-from fastapi import APIRouter
-
-
-index_router = APIRouter()
+from api.v1.app import index_router
 
 
 @index_router.get("/status")
 def status():
     """Returns the status of the API"""
     return {"status": "ok"}
-
-
-app.include_router(index_router, prefix="/api/v1")
