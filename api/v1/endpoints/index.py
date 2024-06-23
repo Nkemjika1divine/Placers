f902055aa263312feb7b613efe7f9dc2582c645c
Nkemjika1divine <cndivine@gmail.com>
2024-06-23 20:28:43 -0300

    Api routes /status, /unauthorized, /forbidden added

--- api/v1/endpoints/index.py
+++ api/v1/endpoints/index.py
@@ -1,9 +1,36 @@
 #!/usr/bin/python3
 """Module conntaining index endpoints"""
-from api.v1.app import index_router
+#!/usr/bin/python3
+"""Module deploying our FastAPI app"""
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+from api.v1.error_handlers import Unauthorized, Forbidden
+# from starlette.status import HTTP_401_UNAUTHORIZED
+# from api.v1.endpoints.index import index_router
+
+
+
+index_router = APIRouter()
+
+
+
+"""@app.exception_handler(Unauthorized)
+def handle_unauthorized():
+    return JSONResponse({"error": "Unauthorized"}, status_code=HTTP_401_UNAUTHORIZED)"""
+
 
 
 @index_router.get("/status")
 def status():
     """Returns the status of the API"""
-    return {"status": "ok"}
+    return JSONResponse({"status": "ok"})
+
+@index_router.get("/unauthorized")
+def unauthorized():
+    """Raises Error 401 (Unauthorized)"""
+    raise Unauthorized()
+
+@index_router.get("/forbidden")
+def forbidden():
+    """Raises Error 403 (Forbidden)"""
+    raise Forbidden()
--- api/v1/error_handlers.py
+++ api/v1/error_handlers.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 """Module containing error handling classes"""
+from typing import Any, Dict
+from typing_extensions import Annotated, Doc
 from fastapi import HTTPException
-from starlette.status import HTTP_401_UNAUTHORIZED
+from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_403_FORBIDDEN
 
 
 class Unauthorized(HTTPException):
     """Handles Unauthorized access"""
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__(status_code=HTTP_401_UNAUTHORIZED, detail={"error": "Unauthorized"})
+
+
+class Forbidden(HTTPException):
+    """Handles Forbidden access"""
+    def __init__(self) -> None:
+        super().__init__(status_code=HTTP_403_FORBIDDEN, detail={"error": "Forbidden"})
