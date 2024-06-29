18753276f26e9ef2b35c7afe24e7948b68db1833
Nkemjika1divine <cndivine@gmail.com>
2024-06-29 13:45:42 -0300

    BasicAuth.decode_base64_authorization_header added

--- auth/basic_auth.py
+++ auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Basic Authentication module"""
 from auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -16,4 +17,13 @@ class BasicAuth(Auth):
             return authorization_header[6:]
         return None
     
-    
+    def decode_base64_authorization_header(self, base64_header: str) -> str:
+        """Decodes the meaning of the base64 part of the authorization header"""
+        if not base64_header:
+            return None
+        if type(base64_header) is not str:
+            return None
+        try:
+            return base64.b64decode(base64_header.encode()).decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
