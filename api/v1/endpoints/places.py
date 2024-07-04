e75a1a53adb530bf9ad79370512cff73471bc9d2
Nkemjika1divine <cndivine@gmail.com>
2024-07-03 22:07:07 -0300

    DELETE /api/v1/places/<place_id> added

--- api/v1/endpoints/places.py
+++ api/v1/endpoints/places.py
@@ -6,6 +6,7 @@ from api.v1.error_handlers import Unauthorized, Forbidden, Not_Found, Bad_Reques
 from models.place import Place
 from models.user import User
 from models import storage
+from utils.utility import check_if_word_exists
 
 
 place_router = APIRouter()
@@ -23,6 +24,7 @@ def get_places():
         all_places[key] = value.to_dict()
     return JSONResponse(content=all_places, status_code=status.HTTP_200_OK)
 
+
 @place_router.get("/places/<place_id>")
 def get_a_place(place_id: str = None) -> str:
     """GET request for a particular place"""
@@ -34,25 +36,41 @@ def get_a_place(place_id: str = None) -> str:
             return JSONResponse(content=value.to_json(), status_code=status.HTTP_200_OK)
     raise Not_Found()
 
+
 @place_router.get("/places/search/<keyword>")
-def get_a_place(keyword: str = None) -> str:
+def search_for_a_place(keyword: str = None) -> str:
     """GET request to search for a particular place with a name"""
     if not keyword:
         raise Not_Found()
     place_list = []
     data = storage.all("Place")
     for value in data.values():
-        if value.name == keyword:
+        if check_if_word_exists(keyword, value.name):
             place_list.append(value)
-        elif value.category == keyword:
+        elif check_if_word_exists(keyword, value.category):
             place_list.append(value)
-        elif value.address == keyword:
+        elif check_if_word_exists(keyword, value.address):
             place_list.append(value)
-        elif value.city == keyword:
+        elif check_if_word_exists(keyword, value.city):
             place_list.append(value)
-        elif value.state == keyword:
+        elif check_if_word_exists(keyword, value.state):
             place_list.append(value)
-        elif value.country == keyword:
+        elif check_if_word_exists(keyword, value.country):
             place_list.append(value)
         return JSONResponse(content=place_list, status_code=status.HTTP_200_OK)
+    raise Not_Found()
+
+
+@place_router.delete("/places/<place_id>")
+def delete_a_place(place_id: str = None) -> str:
+    """DELETE method that a place"""
+    if not place_id:
+        raise Not_Found()
+    data = storage.all("Place")
+    if not data:
+        raise Not_Found()
+    for value in data.values():
+        if value.id == place_id:
+            storage.delete(value)
+            return JSONResponse(content={}, status_code=status.HTTP_200_OK),
     raise Not_Found()
